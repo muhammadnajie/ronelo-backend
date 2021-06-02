ee6b58886d2102a3e1af047553bb99aa1cdc110d
Muhammad Najie <muhammadalnajie1@gmail.com>
2021-06-02 07:52:03 +0700

    GET Medicine and POST predict

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ from werkzeug.utils import secure_filename
 from peewee import *
 from utils import *
 import json
-import uuid
+import uuid 
+import numpy as np
+import cv2
+from imutils.object_detection import non_max_suppression
+import pytesseract
+import matplotlib.pyplot as plt
+
 
 
 # __name__ tells that it's the root directory
@@ -111,17 +117,19 @@ def get_medicine_by_name(name):
         data.append(new_data)
     return jsonify({'status': 200, 'data': data})
 
+
 @app.route('/upload_image', methods=['POST'])
 def upload_image():
     uploaded_file = request.files['image']
     # filename = secure_filename(uploaded_file.filename)
+
+    if not allowed_file(uploaded_file.filename):
+        return {'status': 400, 'message': f'{uploaded_file.filename} not allowed'}
+
     ext = os.path.splitext(uploaded_file.filename)[1]
     filename = str(uuid.uuid4().hex) + ext
     target_path = os.path.join(
         app.config['UPLOAD_FOLDER'], filename)
-    
-    if not allowed_file(filename):
-        return {'status': 400, 'message': f'{filename} not allowed'}
     
     if uploaded_file.filename != '':
         try:
@@ -131,5 +139,141 @@ def upload_image():
     return target_path
 
 
+def find_roi(image_loc):
+
+    #Creating argument dictionary for the default arguments needed in the code. 
+    args = {"image":image_loc, "east": "frozen_east_text_detection.pb", "min_confidence":0.9, "width":320, "height":320}
+
+    image = cv2.imread(args['image'])
+
+    #Saving a original image and shape
+    orig = image.copy()
+    (origH, origW) = image.shape[:2]
+
+    # set the new height and width to default 320 by using args #dictionary.  
+    (newW, newH) = (args["width"], args["height"])
+
+    #Calculate the ratio between original and new image for both height and weight. 
+    #This ratio will be used to translate bounding box location on the original image. 
+    rW = origW / float(newW)
+    rH = origH / float(newH)
+
+    # resize the original image to new dimensions
+    image = cv2.resize(image, (newW, newH))
+    (H, W) = image.shape[:2]
+
+    # construct a blob from the image to forward pass it to EAST model
+    blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+	    (123.68, 116.78, 103.94), swapRB=True, crop=False)
+
+    # load the pre-trained EAST model for text detection 
+    net = cv2.dnn.readNet(args["east"])
+
+    # The following two layer need to pulled from EAST model for achieving this. 
+    layerNames = [
+        "feature_fusion/Conv_7/Sigmoid",
+        "feature_fusion/concat_3"]
+    
+    #Forward pass the blob from the image to get the desired output layers
+    net.setInput(blob)
+    (scores, geometry) = net.forward(layerNames)
+
+    (numR, numC) = scores.shape[2:4]
+    boxes = []
+    confidence_val = []
+
+    # loop over rows
+    for y in range(0, numR):
+        scoresData = scores[0, 0, y]
+        x0 = geometry[0, 0, y]
+        x1 = geometry[0, 1, y]
+        x2 = geometry[0, 2, y]
+        x3 = geometry[0, 3, y]
+        anglesData = geometry[0, 4, y]
+
+        # loop over rows
+        for y in range(0, numR):
+            scoresData = scores[0, 0, y]
+            x0 = geometry[0, 0, y]
+            x1 = geometry[0, 1, y]
+            x2 = geometry[0, 2, y]
+            x3 = geometry[0, 3, y]
+            anglesData = geometry[0, 4, y]
+
+            # loop over the number of columns 
+            for i in range(0, numC):
+                if scoresData[i] < args["min_confidence"]:
+                    continue
+
+                (offX, offY) = (i * 4.0, y * 4.0)
+
+                # extracting the rotation angle for the prediction and computing the sine and cosine
+                angle = anglesData[i]
+                cos = np.cos(angle)
+                sin = np.sin(angle)
+
+                # using the geo volume to get the dimensions of the bounding box
+                h = x0[i] + x2[i]
+                w = x1[i] + x3[i]
+
+                # compute start and end for the text pred bbox
+                endX = int(offX + (cos * x1[i]) + (sin * x2[i]))
+                endY = int(offY - (sin * x1[i]) + (cos * x2[i]))
+                startX = int(endX - w)
+                startY = int(endY - h)
+
+                boxes.append((startX, startY, endX, endY))
+                confidence_val.append(scoresData[i])
+
+    # return bounding boxes and associated confidence_val
+    return (boxes, confidence_val, rW, rH, orig)
+
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    image_loc = request.form['image_loc']
+    image_loc = os.path.join(cwd, image_loc)
+
+    (boxes, confidence_val, rW, rH, orig) = find_roi(image_loc)
+    boxes = non_max_suppression(np.array(boxes), probs=confidence_val)
+
+    ##Text Detection and Recognition 
+
+    # initialize the list of results
+    results = []
+
+    for (startX, startY, endX, endY) in boxes:
+	# scale the coordinates based on the respective ratios in order to reflect bounding box on the original image
+        startX = int(startX * rW)
+        startY = int(startY * rH)
+        endX = int(endX * rW)
+        endY = int(endY * rH)
+
+        #extract the region of interest
+        r = orig[startY:endY, startX:endX]
+
+        #configuration setting to convert image to string.  
+        configuration = ("-l eng --oem 1 --psm 8")
+        ##This will recognize the text from the image of bounding box
+        text = pytesseract.image_to_string(r, config=configuration)
+
+        # append bbox coordinate and associated text to the list of results 
+        results.append(((startX, startY, endX, endY), text))
+
+    medicines = []
+    for _, name in results:
+        with app.app_context():
+            medicine = get_medicine_by_name(name[:-2])
+        
+        medicine = json.loads(medicine.data)
+        if medicine['data']:
+            medicines.append(medicine['data'])
+        
+    return jsonify({
+        'status': 200,
+        'data' : medicines
+    })
+    
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
