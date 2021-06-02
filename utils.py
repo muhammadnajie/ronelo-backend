5525d86d1966000ad59e3cdc3367598e9edaaefb
Muhammad Najie <muhammadalnajie1@gmail.com>
2021-06-02 12:23:10 +0700

    Server config

--- utils.py
+++
@@ -1,7 +0,0 @@
-"""CONSTANTS"""
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
